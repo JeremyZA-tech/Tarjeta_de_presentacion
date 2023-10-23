74163be5b40a72b9c8f54fe266b2784cd117c919
JeremyZA-tech <jeremyzamalloaarmas@gmail.com>
2023-10-23 17:45:25 +0200

    Initial commit

--- app/src/main/java/com/example/tarjeta_de_presentacion/MainActivity.kt
+++ app/src/main/java/com/example/tarjeta_de_presentacion/MainActivity.kt
@@ -3,13 +3,24 @@ package com.example.tarjeta_de_presentacion
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.tarjeta_de_presentacion.ui.theme.Tarjeta_de_presentacionTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +33,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    CardPresentation("Android")
+                    CardPresentation()
                 }
             }
         }
@@ -30,27 +41,94 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun CardPresentation(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun CardPresentation(modifier: Modifier = Modifier) {
+    Column(
+       // modifier = modifier.fillMaxSize()
+    ) {
+        Logo(modifier = Modifier.weight(1f))
+        Data(modifier = modifier
+            .fillMaxWidth()
+            .weight(1f)
+            .padding(bottom = 20.dp))
+    }
 }
+
+
 
 @Preview(showBackground = true)
 @Composable
 fun CardPreview() {
     Tarjeta_de_presentacionTheme {
-        CardPresentation("Android")
+        CardPresentation()
     }
 }
 
 @Composable
-fun logo (){
-
+fun Logo(modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.logo)
+    Column(
+        modifier = modifier
+    ) {
+        Image(
+            painter = image,
+            contentDescription = null,
+            modifier = Modifier
+                .size(150.dp)
+        )
+        Text(
+            text = "Jeremy Zamalloa Armas",
+            textAlign = TextAlign.Center,
+            modifier = modifier.padding(5.dp)
+        )
+        Text(
+            text = "Desarrollador de Software",
+            modifier = modifier.padding(5.dp)
+        )
+    }
 }
 
 @Composable
-fun data(){
+fun Data(modifier: Modifier = Modifier){
+    val phone = painterResource(R.drawable.phone)
+    val email = painterResource(R.drawable.email)
+    val share = painterResource(R.drawable.share)
 
+    Column(
+       modifier = Modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Row(
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Image(
+                painter = phone,
+                contentDescription = null,
+            )
+            Text(
+                text = "Telefono",
+                modifier = modifier.padding(5.dp)
+
+            )
+        }
+        Row {
+            Image(
+                painter = share,
+                contentDescription = null,
+            )
+            Text(
+                text = "Redes sociales",
+                modifier = modifier.padding(5.dp)
+            )
+        }
+        Row {
+            Image(
+                painter = email,
+                contentDescription = null,
+            )
+            Text(
+                text = "Email",
+                modifier = modifier.padding(5.dp)
+            )
+        }
+    }
 }
